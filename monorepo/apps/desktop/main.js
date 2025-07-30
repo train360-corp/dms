4a982839631e221aef468c5acb55e51eeb8b969a
Nicholas Barrow <nbarrow@inspiretmstech.com>
2025-07-30 12:35:06 -0400

    feat: monorepo

---
+++ monorepo/apps/desktop/main.js
@@ -0,0 +1,5 @@
+const { app, BrowserWindow } = require('electron')
+
+app.whenReady().then(() => {
+  console.log('Hello from Electron 👋')
+})
--- monorepo/apps/web/dev-entrypoint.js
+++ monorepo/apps/web/dev-entrypoint.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-require("dotenv").config();
+require("dotenv").config({ path: path.join(__dirname, ".env.local") });
 
 
 const env = {
