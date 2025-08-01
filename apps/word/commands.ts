2517cedf843dfbf759ddf672b62452955bb357aa
Nicholas Barrow <nbarrow@inspiretmstech.com>
2025-07-31 22:42:40 -0400

    feat: deploy word to github pages

--- apps/word/commands.ts
+++ apps/word/commands.ts
@@ -1,29 +1,30 @@
 /// <reference types="office-js" />
 
+Office.onReady()
+  .then(() => {
+    console.log("Office is ready");
+    Office.actions.associate("save", save);
+    Office.actions.associate("saveAsNewVersion", saveAsNewVersion);
+    Office.actions.associate("saveAsNewFile", saveAsNewFile);
+  })
+  .catch((e) => {
+    console.error("Office.onReady failed:", e);
+  });
 
-
-Office.onReady().then(() => {
-  console.log("Office is ready");
-
-  // Register command callbacks by name
-  Office.actions.associate("save", save);
-  Office.actions.associate("saveAsNewVersion", saveAsNewVersion);
-  Office.actions.associate("saveAsNewFile", saveAsNewFile);
-}).catch(e => {
-  console.error(e);
-});
-
-function save() {
+function save(): Promise<void> {
   console.log("✅ save() was called");
+  window.location.href = "projdocs://app/1";
   return Promise.resolve();
 }
 
-function saveAsNewVersion() {
+function saveAsNewVersion(): Promise<void> {
   console.log("✅ saveAsNewVersion() was called");
+  window.location.href = "projdocs://app/2";
   return Promise.resolve();
 }
 
-function saveAsNewFile() {
+function saveAsNewFile(): Promise<void> {
   console.log("✅ saveAsNewFile() was called");
+  window.location.href = "projdocs://app/3";
   return Promise.resolve();
 }
